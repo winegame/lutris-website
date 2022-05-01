8c9c34a90ae54a7c95598ef50d466e20b4f63edc
SwimmingTiger <hu60.cn@gmail.com>
2022-05-01 23:35:15 +0800

    YAML生成：让多行文本具有heredoc风格

--- common/util.py
+++ common/util.py
@@ -161,3 +161,16 @@ def crop_banner(img_path, dest_path, banner_size=(184, 69)):
     image = image.crop(box)
     image.mode = "RGB"
     image.save(dest_path, "JPEG")
+
+
+# YAML生成：让多行文本具有heredoc风格
+# From <https://stackoverflow.com/questions/8640959/how-can-i-control-what-scalar-form-pyyaml-uses-for-my-data>
+def str_presenter(dumper, data):
+  if len(data.splitlines()) > 1:  # check for multiline string
+    return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
+  return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+
+yaml.add_representer(str, str_presenter)
+
+# to use with safe_dump:
+yaml.representer.SafeRepresenter.add_representer(str, str_presenter)
